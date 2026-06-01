164c74993bd796a7d4123077ee5d27b28b41dba8
Aftab Shaikh <shaikhaftab7861234@gmail.com>
2026-06-01 18:42:12 +0530

    adding the new changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -80,9 +80,11 @@ document.addEventListener("DOMContentLoaded", function () {
   const navToggle = document.querySelector(".nav-toggle");
   const navLinks = document.querySelector(".navlinks-mobile");
 
-  navToggle.addEventListener("click", function () {
-    navLinks.classList.toggle("active");
-  });
+  if (navToggle && navLinks) {
+    navToggle.addEventListener("click", function () {
+      navLinks.classList.toggle("active");
+    });
+  }
 });
 
 // Toggle Sub Menu
